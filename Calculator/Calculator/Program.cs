17a73a2161a18706a7a4628071a82fcb85f74b58
Emir Sinan Yoldas <yoldasemirsinan@gmail.com>
2024-09-17 14:16:28 +0300

    adding something

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
+    public class Calculator
+    {
+        public double number1 { get; set; }
+        public double number2 { get; set; }
+        public string operation { get; set; }
+
+        private Dictionary<string, Func<double>> operations;
+
+    }
+
+
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
+
     }
 }
