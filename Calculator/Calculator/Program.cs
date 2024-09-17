c661efd24349ce5585e70fb6c2e7c3059884f3fb
Emir Sinan Yoldas <yoldasemirsinan@gmail.com>
2024-09-17 14:39:25 +0300

    calculations added
    bazı işlemleri ekledim

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,10 +10,26 @@ namespace Calculator
     {
         public double number1 { get; set; }
         public double number2 { get; set; }
-        public string operation { get; set; }
+        public string operation1 { get; set; }
 
         private Dictionary<string, Func<double>> operations;
 
+        public Calculator(double num1, double num2,string opert1)
+        {
+            number1 = num1;
+            number2 = num2;
+            operation1 = opert1;
+
+            operations = new Dictionary<string, Func<double>>()
+            {
+                { "+",()=>number1+number2},
+                { "-",()=>number1-number2},
+                { "*",()=>number1*number2},
+                { "/",()=>number2==0?throw new DivideByZeroException("Cannot divide by zero."):number1/number2},
+                { "%",()=>number2==0?throw new DivideByZeroException("Cannot divide by zero."):number1%number2},
+                { "!",()=>Factorial((int)number1)}
+            };
+        }
     }
 
 
